3133d87c899b381eb074a12aec6596a7382c0c59
amrheart <amrheart98@yahoo.com>
2023-05-31 12:12:06 -0400

    got it working and added a console message

--- servertest.js
+++ servertest.js
@@ -1,26 +1,15 @@
 const http = require('http');
-const os = require('os');
 
 const server = http.createServer((req, res) => {
+  const ipAddress = req.connection.remoteAddress;
+  console.log(`User connected from ${ipAddress}`);
+
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, world!\n');
 });
 
 const port = 3000;
-const ipAddress = getIPAddress();
-server.listen(port, ipAddress, () => {
-  console.log(`Server running at http://${ipAddress}:${port}/`);
+const host = '0.0.0.0'
+server.listen(port, host, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
-function getIPAddress() {
-  const interfaces = os.networkInterfaces();
-  for (const interfaceName in interfaces) {
-    const addresses = interfaces[interfaceName];
-    for (const address of addresses) {
-      if (address.family === 'IPv4' && !address.internal) {
-        return address.address;
-      }
-    }
-  }
-  return 'localhost';
-}
