70e0f13351b5fcffaec9bb93c8ef491e7f0e42cf
amrheart <amrheart98@yahoo.com>
2023-05-31 14:09:36 -0400

    attempt to fix object blob

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const http = require('http');
 const fs = require('fs');
+const WebSocket = require('ws');
 
-// Read the HTML file for the chat room page
 const chatRoomPage = fs.readFileSync('chatroom.html', 'utf8');
 
-// Create an array to store the chat messages
 const chatMessages = [];
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    // Serve the chat room page
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(chatRoomPage);
   } else if (req.url === '/messages') {
-    // Handle incoming messages
     if (req.method === 'POST') {
       let body = '';
       req.on('data', chunk => {
@@ -22,30 +19,24 @@ const server = http.createServer((req, res) => {
       req.on('end', () => {
         const message = decodeURIComponent(body);
         chatMessages.push(message);
-        // Broadcast the new message to all connected clients
-        broadcastMessage(message);
+        broadcastMessage(JSON.stringify(message)); // Convert message to string before broadcasting
         res.end();
       });
     } else if (req.method === 'GET') {
-      // Return the chat messages as JSON
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(chatMessages));
     }
   } else {
-    // Handle invalid URLs
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found');
   }
 });
 
 const port = 3000;
-const host = '0.0.0.0'
-server.listen(port, host, () => {
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
 
-// WebSocket implementation for real-time communication
-const WebSocket = require('ws');
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', ws => {
@@ -54,8 +45,7 @@ wss.on('connection', ws => {
   ws.on('message', message => {
     console.log(`Received message: ${message}`);
     chatMessages.push(message);
-    // Broadcast the new message to all connected clients, including the sender
-    broadcastMessage(message);
+    broadcastMessage(JSON.stringify(message)); // Convert message to string before broadcasting
   });
 
   ws.on('close', () => {
@@ -70,3 +60,4 @@ function broadcastMessage(message) {
     }
   });
 }
+
