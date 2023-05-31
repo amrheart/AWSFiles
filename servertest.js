5623caf92cfa1afc09f78ffe06d738d8b577394b
amrheart <amrheart98@yahoo.com>
2023-05-31 11:43:27 -0400

    added server tests

---
+++ servertest.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const os = require('os');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, world!\n');
+});
+
+const port = 3000;
+const ipAddress = getIPAddress();
+server.listen(port, ipAddress, () => {
+  console.log(`Server running at http://${ipAddress}:${port}/`);
+});
+
+function getIPAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+    const addresses = interfaces[interfaceName];
+    for (const address of addresses) {
+      if (address.family === 'IPv4' && !address.internal) {
+        return address.address;
+      }
+    }
+  }
+  return 'localhost';
+}
